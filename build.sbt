f0e6a6efed99cdb7c919619fbb0d8d4e21131d97
anthonysyk <anthony.ssiyk@gmail.com>
2017-06-04 11:30:04 +0200

    add webpack and front dependencies

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := """pop-the-corn"""
 
 version := "1.0-SNAPSHOT"
 
-lazy val root = (project in file(".")).enablePlugins(PlayScala)
+lazy val root = (project in file(".")).enablePlugins(PlayScala, SbtWeb)
 
 scalaVersion := "2.11.7"
 
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "../stylesheets/[name].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+    entry: {
+        main: './app/assets/javascripts/main.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'public/javascripts'),
+        publicPath: 'assets/javascripts/',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [
+                        {loader: 'css-loader'},
+                        {loader: 'resolve-url-loader'},
+                        {loader: 'sass-loader', options: {sourceMap: true}}
+                    ],
+                    fallback: 'style-loader'
+                })
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: process.env.NODE_ENV !== 'development' ? {
+                                name: '../stylesheets/[name].[ext]',
+                                publicPath: './'
+                            } : {}
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [extractSass],
+    devServer: {
+        compress: true,
+        proxy: {
+            '*': {
+                target: 'http://localhost:9000'
+            }
+        }
+    }
+
+
+};
